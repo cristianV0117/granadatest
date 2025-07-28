3f90523dfb05dc4f19252d2d8bd1bb59bbb8c1a7
cristian <cvo6372@gmail.com>
2025-07-28 08:54:06 -0500

    cors modify

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['graphql', 'api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200'],
+    'allowed_origins' => ['https://granadatestfront-production.up.railway.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
